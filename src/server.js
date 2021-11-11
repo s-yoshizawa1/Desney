5890b0a50accd19f2a8e723477b36d13102d9b00
seiya <yoshizawa_seiya@yahoo.co.jp>
2021-11-11 23:47:09 +0900

    method修正

--- src/server.js
+++ src/server.js
@@ -6,12 +6,14 @@ app.set("view engine", "ejs");
 
 
 const setupExpressServer = () => {
+  app.use( express.static( "public" ) );
   app.use(express.json());
 
   //  /v1/api/desney/attraction/list endpoint
   
   app.get( '/', ( req, res ) => {
-    res.render( 'test.ejs', { title: 'Desney' } );  
+    // res.render( 'test.ejs', { title: 'Desney' } );  
+    res.sendFile(__dirname + '/index.html');
   });
 
 // 　一覧を取得する
@@ -21,17 +23,19 @@ const setupExpressServer = () => {
     if(!isNaN(limit)){
       knex("attraction_info")
       .select()
+      .orderBy('theme','average_waiting_time_during_busy_season')
       .then(function (results) {
-      // res.send(results);
-      res.sendFile('/public/test.html');
+      res.send(results);
+      // res.sendFile('/public/test.html');
       });
     res.send(attractionList);
     }else{
       knex("attraction_info")
       .select()
+      .orderBy('theme','average_waiting_time_during_busy_season')
       .then((results) => {
-      // res.send(results);
-      res.sendFile(__dirname + '/public/test.html');
+      res.send(results);
+      // res.sendFile(__dirname + '/public/test.html');
       });
     }
   });
@@ -70,14 +74,40 @@ const setupExpressServer = () => {
 
   //  /v1/api/desney/attraction/waitedtime endpoint
   //　一覧を取得する
-  app.get("/v1/api/desney/attraction/waitedtime/", (req, res) => {
+  // app.get("/v1/api/desney/attraction/waitedtime/", (req, res) => {
   
-      knex("attraction_info")
-      .select("id","attraction_name","congestion_level")
-      .then((results) => {
-      res.send(results);
-      });
-  });
+  //     knex("attraction_info")
+  //     .orderBy('average_waiting_time_during_busy_season')
+  //     .select("id","attraction_name","average_waiting_time_during_busy_season").limit(10)
+  //     .then((results) => {
+  //     res.send(results);
+      // const attractionName = req.body[0].attraction_name;
+      // const congestionLevel = req.body[0].congestion_level;
+
+      // res.render( 'test.ejs',  req.body[0] );  
+  //     });
+  // });
+
+  //　待ち時間が最速のアトラクションを提案する
+  // app.post("/v1/api/desney/attraction/waitedtime/", (req, res) => {
+    // let area = req.body[0].area;
+    // let theme = req.body[0].theme
+  
+    // knex("attraction_info")
+    //   .where({
+    //   area: area,
+    //   theme:  theme
+    //  })
+    //   .select("id","attraction_name","average_waiting_time_during_busy_season","vehicle_capacity","next_interval")
+    //   req.body[0].average_waiting_time_during_busy_season + req.body[0].vehicle_capacity;
+    // .then((results) => {
+    // res.send(results);
+    // const attractionName = req.body[0].attraction_name;
+    // const congestionLevel = req.body[0].congestion_level;
+
+    // res.render( 'test.ejs',  req.body[0] );  
+    // });
+// });
 
   return app;
 };
